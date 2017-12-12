b5fb28820ca44f2ee1d5c380cbb0cc69df6a976e
mfpinheiro <mateusfpinheiro@gmail.com>
2017-12-12 10:47:41 -0200

    :rotating_light: Add test to check if options of amount and currency are avaliable on the CLI.

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -5,10 +5,34 @@ const btcConverter = './src/main.js';
 
 
 describe('Main CLI', () => {
-  it('should return version of btc-conversion', done => {
-    exec(`${btcConverter} --version`, (err, stdout, sterr) => {
+  it('should return version of btc-conversion', (done) => {
+    exec(`${btcConverter} --version`, (err, stdout) => {
       if (err) throw err;
       expect(stdout.replace('\n', '')).to.be.equal(pkg.version);
+      done();
+    });
+  });
+
+  it('should return the description when btc-converter --help', (done) => {
+    exec(`${btcConverter} --help`, (err, stdout) => {
+      if (err) throw err;
+      expect(stdout.includes('Convert Bitcoin to any currency defined')).to.be.true;
+      done();
+    });
+  });
+
+  it('should return currency option when BTC-converter --help', (done) => {
+    exec(`${btcConverter} --help`, (err, stdout) => {
+      if (err) throw  err;
+      expect(stdout.includes('--currency')).to.be.true;
+      done();
+    });
+  });
+
+  it('should return amount option when BTC-coverter --help', (done) => {
+    exec(`${btcConverter} --help`, (err, stdout) => {
+      if (err) throw err;
+      expect(stdout.includes('--amount')).to.be.true;
       done();
     });
   });
