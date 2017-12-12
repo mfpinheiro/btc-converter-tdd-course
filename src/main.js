3635a6c69d88f63066c54967398d3f5c2fb9c915
mfpinheiro <mateusfpinheiro@gmail.com>
2017-12-12 10:49:09 -0200

    :sparkles: Add currency and amount params to the cli comand and --help doc for them.

--- src/main.js
+++ src/main.js
@@ -2,5 +2,13 @@
 
 const program = require('commander');
 const pkg = require('../package.json');
+const convertBTC = require('./convertBTC');
 
-program.version(pkg.version).parse(process.argv);
+program
+  .version(pkg.version)
+  .description('Convert Bitcoin to any currency defined')
+  .option('-C, --currency <currency>', 'Currency to be conveted. (Default: USD)')
+  .option('-A, --amount <ammount>', 'Value in Bitcoin to be converted. (Default: 1)')
+  .parse(process.argv);
+
+console.log(convertBTC(program.currency, program.amount));
