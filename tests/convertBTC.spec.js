e36b8fa6b0b5365eb3ba2cb413495901faf6affd
mfpinheiro <mateusfpinheiro@gmail.com>
2017-12-12 22:10:07 -0200

    :rotating_light: & :sparkles: Add tests to error handler and function to respond with a erro msg if catch something wrong.

--- tests/convertBTC.spec.js
+++ tests/convertBTC.spec.js
@@ -51,4 +51,32 @@ describe('ConvertBTC', () => {
       done();
     }, 300);
   });
+
+  it('should use currency BRL and 1 as amount default', (done) => {
+    nock('https://apiv2.bitcoinaverage.com')
+      .get('/convert/global')
+      .query({ from: 'BTC', to: 'BRL', amount: 1 })
+      .reply(200, responseMock);
+
+    convertBTC('BRL');
+
+    setTimeout(() => {
+      expect(consoleStub).to.have.been.calledWith('1 BTC to BRL = 17103.1');
+      done();
+    }, 300);
+  });
+
+  it('should message the user when api replay with error', (done) => {
+    nock('https://apiv2.bitcoinaverage.com')
+      .get('/convert/global')
+      .query({ from: 'BTC', to: 'BRL', amount: 1 })
+      .replyWithError('Error');
+
+    convertBTC('BRL');
+
+    setTimeout(() => {
+      expect(consoleStub).to.have.been.calledWith('Something went wrong in the API. Try in a few minutes.');
+      done();
+    }, 300);
+  });
 });
